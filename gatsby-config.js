083940064caf2865e4b69599987f3f820011eb5e
Kenneth Wang <kennethwang14@gmail.com>
2019-12-26 23:52:53 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
+require('dotenv').config({
+	path: '.env',
+});
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		siteTitle: 'Gatsby Starter DotDev',
+		siteTitleAlt: 'Gatsby Starter DotDev',
+		siteHeadline: 'Gatsby Starter DotDev',
+		siteUrl: 'https://gatsby-starter-dotdev.kennethwang.dev',
+		siteDescription: 'Gatsby Starter for Dotdev Theme',
+		siteLanguage: 'en',
+		siteImage: '/banner.jpg',
+		author: 'John Doe',
+		role: 'backend engineer',
+		footerLinks: [
+			{
+				title: 'Email',
+				url: 'mailto:me@kennethwang.dev',
+			},
+			{
+				title: 'GitHub',
+				url: 'https://github.com/KennethWangDotDev/gatsby-starter-dotdev',
+			},
+		],
+		projectsSubtitle: 'A selection of my favorite projects, completed in recent years.',
+		writingsSubtitle: 'My thoughts on web development and product design.',
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-theme-dotdev',
+			options: {},
+		},
+	],
+};
